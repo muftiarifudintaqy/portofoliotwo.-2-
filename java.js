7181fbd17c483ba817f4a852af56a810d7e3ff31
muftiarifudintaqy <muftiarifudintaqy0@gmail.com>
2024-09-13 10:43:19 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,40 @@
+let age = 17;
+let years80 = 80 - age;
+console.log(`Kamu punya waktu sekitar ${years80} tahun habis itu baru kamu meninggal.`);
+
+let firstName = "Mufti";
+let lastName = "Arifudin Taqy";
+let birthDay = 6;   
+let birthMonth = 8 +"agustus"; 
+let birthYear = 2007;
+
+console.log(`Nama: ${firstName} ${lastName}, Umur: ${age}, Keahiran-Tanggal: ${birthDay}, Bulan: ${birthMonth}, Tahun: ${birthYear}`);
+ 
+let berusaha ="berusaha sendiri dong jangan pake ai-:AI hanyalah bantuan untuk orang yang lemah";
+ console.log(`lah lu siapa ${berusaha} `);
+// tugas scoree itee
+ let skorIT = 85;  
+let skorBahasaInggris = 72;  
+if (skorIT < 75) {
+  console.log("Skor IT: REMEDIAL");
+} else {
+  console.log("untung sekarang kamu belajar semoga bisa faham yah jadi Skor IT kamu: SANGAT BAIK");
+}
+
+if (skorBahasaInggris < 75) {
+  console.log("skor>B.inggris Lu harus di REMEDIAL NOWW!╭∩╮( •̀_•́ )╭∩╮ᶠᶸᶜᵏᵧₒᵤ!");
+} 
+// skorr ujian yang bagus
+let skorUjian = 85; 
+
+if (skorUjian >= 90) {
+  console.log("Nilai: J");
+} else if (skorUjian >= 87) {
+  console.log("Nilai: B");
+} else if (skorUjian >= 73) {
+  console.log("Nilai: C");
+} else if (skorUjian >= 64) {
+  console.log("Nilai: D");
+} else if(skorUjian >=27) {
+  console.log("Nilai: K");
+}
